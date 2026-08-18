bdc5ffcef02ff5e5226ce7be1251f4ebd01ada23
shubham <walia4shubham@gmail.com>
2026-08-18 22:04:02 +0530

    refactor: remove unused middleware and clean up code structure

--- src/app.js
+++ src/app.js
@@ -3,17 +3,21 @@ import 'dotenv/config';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use( (req, res) => {
-    if(req.path  == '/s'){
- res.send('Hello, RAM!');
-    }else if (req.path  == '/shu'){
- res.send('Hello, SHUIV!');
-    }else{
- res.send('Hello, worldssssssss!');
-    }
+// app.use( (req, res) => {
+//     if(req.path  == '/s'){
+//  res.send('Hello, RAM!');
+//     }else if (req.path  == '/shu'){
+//  res.send('Hello, SHUIV!');
+//     }else{
+//  res.send('Hello, worldssssssss!');
+//     }
+// });
+app.get('/shubham/:param', (req, res) => {
+    console.log(req.params)
+  res.send({'fnmae':'shubham','lastnmae': 'Ram'});
 });
-app.get('/shubham', (req, res) => {
-  res.send('Hello, mF!');
+app.post('/run', (req, res) => {
+  res.send({'fnmae':'shubham','lastnmae': 'Ram'});
 });
 
 app.listen(PORT, () => {
